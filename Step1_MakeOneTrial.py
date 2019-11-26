efd9aef55ec6e42bf40ee88620dba49e0cd2b83b
Rebekah Gelpi <rebekah.gelpi@gmail.com>
2019-11-26 01:43:04 -0500

    Update Step1_MakeOneTrial.py

--- Step1_MakeOneTrial.py
+++ Step1_MakeOneTrial.py
@@ -12,6 +12,7 @@ Use this template script to present one trial with your desired structure
 import numpy as np
 import pandas as pd
 import os, sys
+import random
 from psychopy import visual, core, event, gui, logging
 
 # open a white full screen window
@@ -21,24 +22,41 @@ win = visual.Window(fullscr=True, allowGUI=False, color='white', unit='height')
 # but you can find examples in the tutorial code 
 #trialClock = core.Clock()
 
-#%% up to you!
-# this is where you build a trial that you might actually use one day!
-# just try to make one trial ordering your lines of code according to the 
-# sequence of events that happen on one trial
-# if you're stuck you can use the responseExercise.py answer as a starting point 
+option_chosen = event.getKeys(keyList=('a', 'b'))
+option_A_chosen = 0
+option_B_chosen = 0
 
-# maybe start by making stimulus objects (e.g. myPic = visual.ImageStim(...))  
+option_A_self = [1, 2, 5]
+option_A_other = [5, 10, 15]
 
-# then draw all stimuli
+option_B_self = [5, 10, 15]
+option_B_other = [-10, -15, -20]
 
-# then flip your window
 
-# then record your responses
+option_A = visual.TextStim(win=win, name='option_A',
+    text='   Option A\n\n    You: +'+option_A_self[random.randint(0, 2)]+'\nYour Team: +'+option_A_other[random.randint(0, 2)]+'',
+    font='Arial',
+    pos=(-0.4, -0.2), height=0.1,
+    color='black', depth=0.0);
+option_A.draw()
+option_B = visual.TextStim(win=win, name='option_B',
+    text='    Option B\n\n     You: +'+option_B_self[random.randint(0, 2)]+'\nYour Team: '+option_B_other[random.randint(0, 2)]+'',
+    font='Arial',
+    pos=(0.4, -0.2), height=0.1,
+    color='black', depth=-1.0);
+option_B.draw()
+win.flip()
 
+if option_chosen[0] == 'a':
+    option_A_chosen += 1
+    event.clearEvents()
+if option_chosen[0] == 'b':
+    option_B_chosen += 1
+    event.clearEvents()
 
 #%% Required clean up
 # this cell will make sure that your window displays for a while and then 
 # closes properly
 
-core.wait(2)
+core.wait(3)
 win.close()
