9fe24e9a4291aac9308d6356d210f4888b76718e
Rebekah Gelpi <rebekah.gelpi@gmail.com>
2019-11-26 10:12:56 -0500

    Update Step2_LoopIt.py

--- Step2_LoopIt.py
+++ Step2_LoopIt.py
@@ -21,6 +21,19 @@ win = visual.Window(fullscr=True, allowGUI=False, color='white', unit='height')
 # but you can find examples in the tutorial code 
 #trialClock = core.Clock()
 
+option_chosen = event.getKeys(keyList=('a', 'b'))
+option_A_chosen_own = 0
+option_B_chosen_own = 0
+option_A_chosen_other = 0
+option_B_chosen_other = 0
+
+option_A_self = [1, 2, 5]
+option_A_other = [5, 10, 15]
+
+option_B_self = [5, 10, 15]
+option_B_other = [-10, -15, -20]
+
+trials = 20
 
 #%% your loop here
 # start by copying your one trial here, then identify what needs to be
@@ -32,7 +45,43 @@ win = visual.Window(fullscr=True, allowGUI=False, color='white', unit='height')
 
 
 # make your loop
-for t in ... :
+for t in range(trials):
+    team = 'Your Team'
+    if t >= 10:
+        team = 'Other Team'
+
+    option_A = visual.TextStim(win=win, name='option_A',
+    text='   Option A\n\n    You: +' + str(option_A_self[random.randint(0, 2)]) + '\n' + team + ': +' + str(option_A_other[random.randint(0, 2)]) + '',
+    font='Arial',
+    pos=(-0.4, -0.2), height=0.1,
+    color='black', depth=0.0);
+    option_A.draw()
+    option_B = visual.TextStim(win=win, name='option_B',
+    text='    Option B\n\n     You: +' + str(option_B_self[random.randint(0, 2)]) + '\n' + team + ': ' + str(option_B_other[random.randint(0, 2)]) + '',
+    font='Arial',
+    pos=(0.4, -0.2), height=0.1,
+    color='black', depth=-1.0);
+    option_B.draw()
+    win.flip()
+
+    if option_chosen[0] == 'a':
+        if t >= 10:
+            option_A_chosen_other += 1
+        else:
+            option_A_chosen_own += 1
+        event.clearEvents()
+        option_A.hide()
+        option_B.hide()
+        win.flip()
+    if option_chosen[0] == 'b':
+        if t >= 10:
+            option_B_chosen_other += 1
+        else:
+            option_B_chosen_own += 1
+        event.clearEvents()
+        option_A.hide()
+        option_B.hide()
+        win.flip()
     
     # include your trial code in your loop but replace anything that should 
     # change on each trial with a variable that uses your iterater
